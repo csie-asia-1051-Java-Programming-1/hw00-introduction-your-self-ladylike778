c8220107464bd4bc67e83124c6be32a7e98048e4
ladylike778 <105021037@live.asia.edu.tw>
2016-09-12 16:01:35 +0800

    0912 hw00 submit
    introdution

---
+++ ptc1/src/ptc1.java
@@ -0,0 +1,12 @@
+
+public class ptc1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System/out.print("i`m Ryan\n i`m studying in asia university")
+	}
+
+}
